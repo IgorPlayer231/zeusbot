36464388da304d7ee25ecc4cdf3efc7efc9e7ced
IgorPlayer231 <felip232q@gmail.com>
2023-02-10 01:58:53 -0300

    first commit

---
+++ dono/gitdobot.js
@@ -0,0 +1,7 @@
+const gitdobot = (prefix) => {
+return`
+*SÓ NO INSTAGRAM DO MEU CRIADOR ^-^ LÁ TEM O VÍDEO BURLANDO SITES E ETC :) https://www.instagram.com/igor_arts_design/
+`
+}
+
+exports.gitdobot = gitdobot
